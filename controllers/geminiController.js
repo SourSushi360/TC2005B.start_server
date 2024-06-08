0e58299e6e1f515f065c49f19dd5dd9a1bc0034e
SourSushi360 <jplopez365@hotmail.com>
2024-06-07 20:53:19 -0600

    updated gemini

--- controllers/geminiController.js
+++ controllers/geminiController.js
@@ -1,18 +1,22 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require('dotenv').config();
 
-async function getResponseChatGemini(req,res) {
+async function getResponseChatGemini(req, res) {
     const { prompt } = req.body;
     const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+    
+    const context = `Eres un cocinero experto. Solo puedes contestar preguntas relacionadas a cocinar o nutrientes. ${prompt}`;
+
     try {
-        const result = await model.generateContent(prompt);
+        const result = await model.generateContent(context);
         const response = await result.response;
         const text = response.text();
         
         return res.json({ response: text });
     } catch (error) {
         console.log(error);
+        res.status(500).send(error);
     }
 }
 
